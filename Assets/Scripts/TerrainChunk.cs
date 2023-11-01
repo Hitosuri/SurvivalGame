3ed5ba57ac83b6faf933fe2f19a4a1e0360a8ef9
QuanLe <zedovblack@gmail.com>
2023-11-01 23:24:47 +0700

    change terrain generation implementation

--- Assets/Scripts/TerrainChunk.cs
+++ Assets/Scripts/TerrainChunk.cs
@@ -6,118 +6,83 @@ namespace Assets.Scripts {
     public class TerrainChunk {
         public Vector2Int coord;
         public Bounds bounds;
-        public Tilemap tilemap;
         public int size;
-        public Func<float, TileBase> selectTileType;
-        public Action<object> lk;
+        public Action<object> print;
+        private Transform observer;
+        private float allowdRadius;
 
-        public bool IsVisible {
-            get => tilemap.gameObject.activeSelf;
-            set => tilemap.gameObject.SetActive(value);
-        }
+        private TerrainTileType[] terrainTileTypes;
+        private Vector3Int[][] tilePositions = null;
+
+        public bool IsVisible { get; private set; }
 
         public TerrainChunk(
             Vector2Int coord,
             int size,
-            Transform parent,
-            Action<Action<float[,]>, Vector2> requestMapAction,
-            Func<float, TileBase> selectTileType,
-            Action<object> lk
+            Action<Action<Vector3Int[][]>, Vector2Int> requestMapAction,
+            Action<object> print,
+            TerrainTileType[] terrainTileTypes,
+            Transform observer,
+            float allowdRadius
         ) {
             this.coord = coord;
             this.size = size;
-            this.selectTileType = selectTileType;
-            this.lk = lk;
+            this.print = print;
+            this.observer = observer;
+            this.terrainTileTypes = terrainTileTypes;
+            this.allowdRadius = allowdRadius;
 
             var position = coord * size;
             bounds = new Bounds(new Vector3(position.x, position.y), Vector2.one * size);
-            var gameObject = new GameObject($"{coord.x}_{coord.y}") {
-                transform = {
-                    position = Vector3.zero
-                },
-            };
-            tilemap = gameObject.AddComponent<Tilemap>();
-            var tilemapRenderer = gameObject.AddComponent<TilemapRenderer>();
-            if (Mathf.Abs(coord.x % 2) - Mathf.Abs(coord.y % 2) == 0) {
-                tilemapRenderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
-                tilemapRenderer.sortingOrder = 100;
-                var maskObject = new GameObject("mask") {
-                    transform = {
-                        position = new Vector3(position.x + (size / 2f), position.y + (size / 2f)),
-                        localScale = new Vector3(size, size),
-                        parent = gameObject.transform,
-                    },
-                };
-
-                var spriteMask = maskObject.AddComponent<SpriteMask>();
-                spriteMask.sprite = Resources.Load<Sprite>("BL_Cement00_1_dummy_7");
-            }
-
-            tilemap.transform.parent = parent;
             IsVisible = false;
             requestMapAction(OnMapDataReceived, position);
         }
 
-        private void OnMapDataReceived(float[,] heightMap) {
-            for (int y = 0; y < size; y++) {
-                for (int x = 0; x < size; x++) {
-                    TileBase tile = selectTileType.Invoke(heightMap[x, y]);
-                    if (tile == null) {
-                        continue;
-                    }
-                    tilemap.SetTile(new Vector3Int(coord.x * size + x, coord.y * size + y), tile);
-                    if (x == 0 && y == 0) {
-                        tilemap.SetTile(new Vector3Int(coord.x * size + x - 1, coord.y * size + y -1), tile);
-                    }
-
-                    if (x == 0 && y == size - 1) {
-                        tilemap.SetTile(new Vector3Int(coord.x * size + x - 1, coord.y * size + y + 1), tile);
-                    }
-
-                    if (x == size - 1 && y == 0) {
-                        tilemap.SetTile(new Vector3Int(coord.x * size + x + 1, coord.y * size + y - 1), tile);
-                    }
-
-                    if (x == size - 1 && y == size - 1) {
-                        tilemap.SetTile(new Vector3Int(coord.x * size + x + 1, coord.y * size + y + 1), tile);
-                    }
-
-                    if (y == 0) {
-                        TileBase g = selectTileType.Invoke(heightMap[x, y]);
-                        if (g == tile) {
-                            tilemap.SetTile(new Vector3Int(coord.x * size + x, coord.y * size + y - 1), tile);
-                        }
-                    }
-
-                    if (y == size - 1) {
-                        TileBase g = selectTileType.Invoke(heightMap[x, y]);
-                        if (g == tile) {
-                            tilemap.SetTile(new Vector3Int(coord.x * size + x, coord.y * size + y + 1), tile);
-                        }
-                    }
-                    
-                    if (x == 0) {
-                        TileBase g = selectTileType.Invoke(heightMap[x, y]);
-                        if (g == tile) {
-                            tilemap.SetTile(new Vector3Int(coord.x * size + x - 1, coord.y * size + y), tile);
-                        }
-                    }
-                    
-                    if (x == size - 1) {
-                        TileBase g = selectTileType.Invoke(heightMap[x, y]);
-                        if (g == tile) {
-                            tilemap.SetTile(new Vector3Int(coord.x * size + x + 1, coord.y * size + y), tile);
-                        }
-                    }
+        private void ShowChunk() {
+            if (IsVisible) {
+                return;
+            }
+            IsVisible = true;
+            for (int i = 0; i < terrainTileTypes.Length; i++) {
+                if (tilePositions[i].Length == 0) {
+                    break;
                 }
+                terrainTileTypes[i].tilemap.SetTiles(tilePositions[i], terrainTileTypes[i].tiles);
             }
         }
 
-        public bool UpdateVisibility(Vector2 observer, float allowdRadius) {
-            float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(observer));
+        private void HideChunk() {
+            if (!IsVisible) {
+                return;
+            }
+
+            IsVisible = false;
+            for (int i = 0; i < terrainTileTypes.Length; i++) {
+                terrainTileTypes[i].tilemap.SetTiles(tilePositions[i], TerrainGenerator.emptyTiles);
+            }
+        }
+
+        private void OnMapDataReceived(Vector3Int[][] data) {
+            tilePositions = data;
+            CheckLoad(GameManager.Instance);
+        }
+
+        public void CheckLoad(GameManager manager) {
+            if (tilePositions == null) {
+                return;
+            }
+            float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(observer.position));
             bool visible = viewerDstFromNearestEdge <= allowdRadius;
-            IsVisible = visible;
-            return visible;
+
+            if (visible && !IsVisible) {
+                ShowChunk();
+                GameManager.Instance.OneSecondTick += CheckLoad;
+            }
+
+            if (!visible && IsVisible) {
+                GameManager.Instance.OneSecondTick -= CheckLoad;
+                HideChunk();
+            }
         }
     }
 }
